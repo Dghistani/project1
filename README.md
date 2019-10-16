19157c83624b536781a114d7c9106998b275cc51
Dag <Dghistania@merrimack.edu>
2019-10-16 18:54:32 +0300

    finish my lovly game

---
+++ README.md
@@ -0,0 +1,48 @@
+# Tic Tac Toe
+It's a Tic Tac Toe game. Very famous game that everyone knows, it must play with two players. One get an X and One get an O. In order to win, you need to get 3 Xs or Os in a row.
+## Description
+
+It is a game for people to have fun. It is a basic game but very fun game and easy to understand.
+
+### Technical Used
+
+HTML
+CSS
+JavaScript, and jQuery
+
+
+### Wireframes
+
+Your step by step planning sketch of your project, that you can post them as an image in here.
+
+### User Stories
+
+As a coustmer I want to know If I won.
+As a coustmer I want to know when its draw.
+As a coustmer I want to reset the game whenever I want.
+As a coustmer I want very good background.
+As a coustmer I want to know the instrouctions of the game.
+
+---
+
+## Planning and Development Process
+
+First day I had a hard time to understand the logic, because when you see the game from outside, you think it is a really easy game, but when you want to develope it and create it,  gets very complicated. Second day I did the table and put the X by click. third day I completed to let two players play and also tells you  when you win or draw. Last thing I did the css to make it pretty and comfortable for eyes
+
+### Problem-Solving Strategy
+
+For me I foucs to see if there is any syntax error and then I put console.log or I google it.
+
+### Unsolved problems
+
+I just didnt do the AI part
+
+---
+
+## Acknowledgments
+
+
+
+---
+
+ ## References
---
+++ main.js
@@ -0,0 +1,86 @@
+let win = [
+  ["1", "2", "3"], ["4", "5", "6"], ["7", "8", "9"],
+  ["1", "4", "7"], ["2", "5", "8"], ["3", "6", "9"],
+  ["1", "5", "9"], ["3", "5", "7"]
+]
+i = 0
+let player1 = []
+let player2 = []
+var click =0
+var win1 = false
+// function 
+var flag = true
+$(".button").click(function (e) {
+
+  $(".col").html("");
+  $(".col").removeClass("clicked");
+
+  player1 = []
+  player2 =[]
+
+  flag = true
+  click =0
+win1 = false
+
+
+  $('.col').css('pointer-events','auto')
+
+
+})
+
+// players
+$(".col").click(function (e) {
+
+let x = false;
+  if (!$(this).hasClass("clicked")) {
+    if (flag) {
+      $(this).html("X");
+      $(this).addClass("clicked");
+      player1.push(e.target.id)
+  flag = !flag
+      win.forEach(ele => {
+        if (player1.includes(ele[0]) && player1.includes(ele[1]) && player1.includes(ele[2]) ){
+          Swal.fire(
+            'Player X won!!'
+          )
+          win1 = true
+          $('.col').css('pointer-events','none')        }
+      });
+
+    }
+
+    else  {
+      $(this).html("O");
+      $(this).addClass("clicked");
+      player2.push(e.target.id)
+      flag = !flag
+      
+      win.forEach(ele => {
+        if (player2.includes(ele[0]) && player2.includes(ele[1]) && player2.includes(ele[2]) ){
+
+          Swal.fire(
+            'Player O won!!'
+          )
+          // x = true
+          $('.col').css('pointer-events','none')
+                }
+
+      });
+
+    }
+    click ++
+    console.log(click)
+    if (click == 9 && !win1){
+
+      Swal.fire(
+        'Draw!!'
+      )
+    }
+  }
+ 
+
+  
+ 
+
+
+});
